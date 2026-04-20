8a4731e230a56a9c6c7f0a024f7872f091ed0f3e
Marcelo Ricardo Griebeler <marcelo.r.griebeler@gmail.com>
2026-04-20 11:45:06 +0200

    Refactores tire- and vehicle-parameter definitions as Vehicle and Tire classes

--- double_track_model.py
+++ double_track_model.py
@@ -1,38 +1,60 @@
 import numpy as np
 
-#1 Vehicle parameters:
+class Vehicle:
+    def __init__(
+        self,
+        track_back_m: int = 1.5,
+        track_front_m: int = 1.5,
+        cg_to_front_m: int = 2,
+        cg_to_rear_m: int = 2,
+        mass_kg: int = 1000,
+        yaw_inertia_multiplier: int = 1,
+        steer_ratio: int = 1,
+        toe_front_deg: int = 0,
+        toe_back_deg:  int = 0,
+        ackermann_factor: int = 1
+        ):
 
-#1.1 Dimensions
-track_back_m = 1.5
-track_front_m = 1.5
-cg_to_front_m = 2
-cg_to_rear_m = 2
+        self.track_back_m = track_back_m
+        self.track_front_m = track_front_m
+        self.cg_to_front_m = cg_to_front_m
+        self.cg_to_rear_m = cg_to_rear_m
+        self.wheelbase_m = cg_to_front_m + cg_to_rear_m
+        self.mass_kg = mass_kg
+        self.yaw_inertia_multiplier = yaw_inertia_multiplier
+        
+        self.yaw_inertia_kgm2 = self.calculate_inertia(
+            track_front_m,
+            track_back_m,
+            self.wheelbase_m,
+            mass_kg,
+            yaw_inertia_multiplier)
+        
+        self.steer_ratio = steer_ratio
+        self.toe_front_deg = toe_front_deg
+        self.toe_front_rad = np.deg2rad(toe_front_deg)
+        self.toe_back_deg = toe_back_deg
+        self.toe_back_rad = np.deg2rad(toe_back_deg)
+        self.ackermann_factor = ackermann_factor
 
-#1.2 Inertia
-mass_kg = 1000
-yaw_inertia_multiplier = 1
+    def calculate_inertia(
+        track_front_m: int,
+        track_back_m: int,
+        wheelbase_m: int,
+        mass_kg: int,
+        yaw_inertia_multiplier: int
+        ) -> int:
+        '''
+        Yaw inertia is calculated based on the moment of inertia of a rectangle
+        with the previously informed dimensions
+        '''
+        track_mean_m = np.mean(track_front_m, track_back_m)
+        yaw_inertia_kgm2 = 1/12*mass_kg*(track_mean_m**2 + wheelbase_m**2)
+        yaw_inertia_kgm2 = yaw_inertia_multiplier*yaw_inertia_kgm2
 
-#1.3 Steering system
-steer_ratio = 1
-toe_front_deg = 0
-toe_back_deg = 0
-ackermann_factor = 1
-'''
-1 = 100% Ackermann
-0 = Parallel steering
--1 = 100% Anti-Ackermann
-'''
+        return yaw_inertia_kgm2
 
-# Yaw Inertia Calculation
-track_mean_m = np.mean(track_front_m, track_back_m)
-wheelbase = cg_to_front_m + cg_to_rear_m
-yaw_inertia_kgm2 = 1/12*mass_kg*(track_mean_m**2 + wheelbase**2)
-yaw_inertia_kgm2 = yaw_inertia_multiplier*yaw_inertia_kgm2
-'''
-Yaw inertia is calculated based on the moment of inertia of a rectangle
-with the previously informed dimensions
-'''
-
-#2 Tire parameters
-cornering_stiffness_NperRad = 30000
-relaxation_length_m = 5
+class Tire:
+    def __init__(self, cornering_stiffness_NperRad, relaxation_length_m):
+        cornering_stiffness_NperRad: int = 3000
+        relaxation_length_m: int = 5
