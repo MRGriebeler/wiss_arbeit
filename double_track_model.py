9bbf2aa0e70fac7a15d91264a3bb234a0e9200d2
Marcelo Ricardo Griebeler <marcelo.r.griebeler@gmail.com>
2026-05-20 14:54:46 +0200

    Added first version of the function veisualize_vehicle_state()

--- double_track_model.py
+++ double_track_model.py
@@ -1,19 +1,20 @@
 import numpy as np
 from scipy.optimize import root
+import matplotlib.pyplot as plt
 
 class Vehicle:
     def __init__(
         self,
-        track_back_m: int = 1.5,
-        track_front_m: int = 1.5,
-        cg_to_front_m: int = 2,
-        cg_to_back_m: int = 2,
-        mass_kg: int = 1000,
-        yaw_inertia_multiplier: int = 1,
-        steer_ratio: int = 1,
-        toe_front_deg: int = 0,
-        toe_back_deg:  int = 0,
-        ackermann_factor: int = 1
+        track_back_m: float = 1.5,
+        track_front_m: float = 1.5,
+        cg_to_front_m: float = 2.0,
+        cg_to_back_m: float = 2.0,
+        mass_kg: float = 1000,
+        yaw_inertia_multiplier: float = 1.0,
+        steer_ratio: float = 1.0,
+        toe_front_deg: float = 0.0,
+        toe_back_deg:  float = 0.0,
+        ackermann_factor: float = 1.0
         ):
 
         self.track_back_m = track_back_m
@@ -40,12 +41,12 @@ class Vehicle:
 
     def calculate_inertia(
         self,
-        track_front_m: int,
-        track_back_m: int,
-        wheelbase_m: int,
-        mass_kg: int,
-        yaw_inertia_multiplier: int
-        ) -> int:
+        track_front_m: float,
+        track_back_m: float,
+        wheelbase_m: float,
+        mass_kg: float,
+        yaw_inertia_multiplier: float
+        ) -> float:
         '''
         Yaw inertia is calculated based on the moment of inertia of a rectangle
         with the previously informed dimensions
@@ -228,15 +229,63 @@ class Simulation:
         self.force_centripetal_N = \
             force_norm_fl + force_norm_fr + force_norm_bl + force_norm_br
 
+    def visualize_vehicle_state(self):
+        
+        rot_x_pts = lambda x,y,ang: x*np.cos(ang) - y*np.sin(ang)
+        rot_y_pts = lambda x,y,ang: x*np.sin(ang) + y*np.cos(ang)
+
+        local_variables = locals()
+        if 'fig' not in local_variables and 'ax' not in local_variables:
+            fig, ax = plt.subplots()
+            plt.show()
+
+        a = self.car.cg_to_front_m
+        b = self.car.cg_to_back_m
+        wf = self.car.track_front_m
+        wb = self.car.track_back_m
+
+        body_pts_x_m = np.array([-b, -b, -b, a, a, a])
+        body_pts_y_m = np.array([-wb/2, +wb/2, 0.0, 0.0, -wf/2, +wf/2])
+
+        x_in_glob_ref = \
+            rot_x_pts(body_pts_x_m, body_pts_y_m, np.pi/2 + self.side_slip_rad)
+        y_in_glob_ref = \
+            rot_y_pts(body_pts_x_m, body_pts_y_m, np.pi/2 + self.side_slip_rad)
+
+        inputs = (x_in_glob_ref, y_in_glob_ref)
+        body_lines = ax.plot(*inputs)
+
+        wheel_pts_x_m = np.array([+0.5, +0.5, -0.5, -0.5, +0.5])
+        wheel_pts_y_m = np.array([-0.2, +0.2, +0.2, -0.2, -0.2])
+        wheel_angle_rad = [self.steer_fl_rad, self.steer_fr_rad, 0, 0]
+
+        wheel_lines = list()
+        for x_shift, y_shift, ang in zip([+a,+a,-b,-b],
+                                         [+wf/2,-wf/2,+wb/2,-wb/2],
+                                         wheel_angle_rad):
+            
+            x_in_car_ref = x_shift + \
+                rot_x_pts(wheel_pts_x_m, wheel_pts_y_m, ang)
+            y_in_car_ref = y_shift + \
+                rot_y_pts(wheel_pts_x_m, wheel_pts_y_m, ang)
+            
+            x_in_glob_ref = \
+                rot_x_pts(x_in_car_ref, y_in_car_ref, np.pi/2 + self.side_slip_rad)
+            y_in_glob_ref = \
+                rot_y_pts(x_in_car_ref, y_in_car_ref, np.pi/2 + self.side_slip_rad)
+
+            inputs = (x_in_glob_ref, y_in_glob_ref)
+            wheel_lines.append(ax.plot(*inputs))
+
     def find_stationary_point(self,
                               state1_name: str,
-                              state1_value_SI: int,
+                              state1_value_SI: float,
                               state2_name: str,
-                              state2_value_SI: int,
+                              state2_value_SI: float,
                               state3_name: str = None,
-                              state3_initial_guess_SI: int = None,
+                              state3_initial_guess_SI: float = None,
                               state4_name: str = None,
-                              state4_initial_guess_SI: int = None):
+                              state4_initial_guess_SI: float = None):
         
         def validate_state_input(input, valid_state_names):
             """ Validate whether 'state' has unique partial 
@@ -290,7 +339,7 @@ class Simulation:
         setattr(self, states_dict[state2_name], state2_value_SI)
 
         initial_guesses_dict = {
-            "radius": 30,
+            "radius": 30.0,
             "sideslip": np.deg2rad(-5),
             "velocity": 50/3.6,
             "steering": np.deg2rad(20),
@@ -317,10 +366,11 @@ class Simulation:
         x0 = [state3_initial_guess_SI, state4_initial_guess_SI]
         
         sol = root(objective_function, x0 = x0)
+        self.visualize_vehicle_state()
         print(f"Root found at: sol.x")
 
 if __name__ == "__main__":
-    car = Vehicle(cg_to_front_m=2, cg_to_back_m=2)
+    car = Vehicle(cg_to_front_m=2.0, cg_to_back_m=2.0)
     tire_front = Tire(cornering_stiffness_NperRad=20000)
     tire_rear = Tire()
     simulation = Simulation(car=car,
@@ -328,6 +378,6 @@ if __name__ == "__main__":
                             tire_rear=tire_rear)
     
     simulation.find_stationary_point(state1_name="steer",
-                                     state1_value_SI=np.deg2rad(10),
+                                     state1_value_SI=np.deg2rad(30),
                                      state2_name="vel",
-                                     state2_value_SI=70/3.6)
+                                     state2_value_SI=20/3.6)
